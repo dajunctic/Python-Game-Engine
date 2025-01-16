a78cccecc16e817db2145b3a74154371d42c4ec7
dajunctic <59690194+dajunctic@users.noreply.github.com>
2025-01-16 23:59:40 +0700

    Oh shit

--- main.py
+++ main.py
@@ -2,46 +2,57 @@ import pygame as pg
 import moderngl as mgl
 import sys
 from model import *
+from camera import *
+
 
 class GraphicsEngine:
-	def __init__(self, win_size=(1600, 900)):
-		pg.init()
+    def __init__(self, win_size=(1280, 720)):
+        pg.init()
 
-		self.WIN_SIZE = win_size
-		pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-		pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
-		pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        self.WIN_SIZE = win_size
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
 
-		pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
-		pg.display.set_caption("Unity Python")
+        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        pg.display.set_caption("Unity Python")
 
-		self.ctx = mgl.create_context()
+        self.ctx = mgl.create_context()
+        # self.ctx.front_face = 'cw'
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
 
-		self.clock = pg.time.Clock()
-		self.scene = Triangle(self)
+        self.clock = pg.time.Clock()
+        self.time = 0
+
+        self.camera = Camera(self)
+        self.scene = Cube(self)
 
 
-	def check_events(self):
-		for e in pg.event.get():
-			if e.type == pg.QUIT or (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
-				self.scene.destroy()
-				pg.quit()
-				sys.exit()
+    def check_events(self):
+        for e in pg.event.get():
+            if e.type == pg.QUIT or (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
+                self.scene.destroy()
+                pg.quit()
+                sys.exit()
 
-	def render(self):
-		self.ctx.clear(color=(0.08, 0.16, 0.18))
-		
-		self.scene.render()
-		# swap buffer
-		pg.display.flip()
+    def render(self):
+        self.ctx.clear(color=(0.08, 0.16, 0.18, 1))
 
-	def run(self):
-		while True:
-			self.check_events()
-			self.render()
-			self.clock.tick(60)
+        self.scene.render()
+        # swap buffer
+        pg.display.flip()
+
+    def get_time(self):
+        self.time = pg.time.get_ticks() * 0.001
+
+    def run(self):
+        while True:
+            self.get_time()
+            self.check_events()
+            self.render()
+            self.clock.tick(60)
+
 
 if __name__ == '__main__':
-	app = GraphicsEngine()
-	app.run()
-
+    app = GraphicsEngine()
+    app.run()
